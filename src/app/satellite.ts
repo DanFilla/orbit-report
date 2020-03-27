c6729fd48fc15c63112e9ea84b5fd2e87d1805ae
Dan Filla <danfilla@gmail.com>
2020-03-27 11:54:15 -0500

    part 6 complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,5 +1,11 @@
 export class Satellite {
 
+  'name': string;
+  'orbitType': string;
+  'type': string;
+  'operational': boolean;
+  'launchDate': string;
+
   constructor(name: string,
     type: string,
     launchDate: string,
@@ -14,10 +20,12 @@ export class Satellite {
       this.operational = operational;
   }
 
-  'name': string;
-  'orbitType': string;
-  'type': string;
-  'operational': boolean;
-  'launchDate': string;
+  shouldShowWarning(): boolean {
+    if (this.type.toLowerCase() === 'space debris') {
+      return true;
+    }else {
+      return false;
+    }
+  }
 
 }
